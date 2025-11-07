d42e7a184d11480154d86cae6a9b41f97a292069
gibranrafii <gibranrafi21@gmail.com>
2025-11-07 14:28:26 +0700

    membuat scaffold dan memanggil FormIsian dari View

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -1,9 +1,15 @@
 package com.example.praktikum5
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikum5.view.FormIsian
+import com.example.praktikum5.view.TampilData
 
 enum class Navigasi {
     Formulirku,
@@ -15,5 +21,20 @@ fun DataApp (
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
 
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    //pilihanjk = jenisK.map { id -> context.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
 }
