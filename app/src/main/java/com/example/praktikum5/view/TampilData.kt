0dae2151b94aa343b26bb18d2f0d0b6e9778f5da
gibranrafii <gibranrafi21@gmail.com>
2025-11-07 14:17:17 +0700

    membuat scaffold di tampil data

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -2,7 +2,14 @@ package com.example.praktikum5.view
 
 import com.example.praktikum5.R
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,6 +22,14 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
         Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id= R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id= R.color.teal_700))
+            )
+        })
+    {
 
-
+    }
 }
