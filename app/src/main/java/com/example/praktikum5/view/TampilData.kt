b9b56084184ff6074b7296926934ebe140d13615
gibranrafii <gibranrafi21@gmail.com>
2025-11-07 14:15:08 +0700

    membuat items tampilan

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -1,12 +1,20 @@
 package com.example.praktikum5.view
 
+import com.example.praktikum5.R
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-    onBackBtnClick: () -> Unit
-) {
+    onBackBtnClick:()->Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
+        Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
+    )
+
 
 }
